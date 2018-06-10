00130c690ee985341cc4b2751e2dffb2ed0c27eb
Hordur Freyr Yngvason <hordurf@gmail.com>
2018-06-10 21:24:38 +0200

    Add match? method

--- lib/rure.rb
+++ lib/rure.rb
@@ -6,15 +6,21 @@ module Rure
   class Regex
     def initialize(pattern)
       @pattern = pattern
-      @pointer = CRure.rure_compile_must(pattern)
-      ObjectSpace.define_finalizer(self, proc { CRure.rure_free(@pointer)})
+      @c_rure = CRure.rure_compile_must(pattern)
+      ObjectSpace.define_finalizer(self, proc { CRure.rure_free(@c_rure)})
+    end
+
+    def match?(string)
+      data = FFI::MemoryPointer.new(:uint8, string.bytesize)
+      data.put_bytes(0, string)
+      CRure.rure_is_match(@c_rure, data, data.size, 0)
     end
   end
 
   module CRure
     extend FFI::Library
     # FIXME: Bundle lib as part of gem
-    ffi_lib "/usr/local/lib64/librure.so"
+    ffi_lib "librure"
 
     class RureMatch < FFI::Struct
       layout \
@@ -26,7 +32,7 @@ module Rure
     attach_function :rure_compile, [:pointer, :size_t, :size_t, :uint32, :pointer, :pointer], :pointer
     attach_function :rure_free, [:pointer], :void
 
-    attach_function :rure_is_match, [:pointer, :pointer, :size_t, :size_t]
-    attach_function :rure_find, [:pointer, :pointer, :size_t, :size_t, :pointer]
+    attach_function :rure_is_match, [:pointer, :pointer, :size_t, :size_t], :bool
+    attach_function :rure_find, [:pointer, :pointer, :size_t, :size_t, :pointer], :bool
   end
 end
